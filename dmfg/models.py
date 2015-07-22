a5917a2aa38599e43e1afe94b923f98f6a519a42
Lee Ingram <lee@soylentcola.com>
2015-07-21 23:14:47 -0700

    Adding owned items for players (such aas ingredients, etc)

--- dmfg/models.py
+++ dmfg/models.py
@@ -83,11 +83,11 @@ class User(db.Model):
     mfg_jobs = db.relationship("ManufactureJob", backref="user")
     items_owned = db.Column(db.String())
 
-    def __init__(self, name="",email="",items_owned=dict()):
+    def __init__(self, name="",email=""):
         self.name = name
         self.created_date = datetime.datetime.now()
         self.email = email
-        self.items_owned = items_owned
+        self.items_owned = dict()
 
     def __repr__(self):
         return '<user id %r name=%r>' % (self.id, self.name)
