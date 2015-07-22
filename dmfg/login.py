18a930f810e3671b6ef55f4c903c0d8dc300a058
Lee Ingram <lee@soylentcola.com>
2015-07-21 17:09:28 -0700

    Removing login.py

--- dmfg/login.py
+++
@@ -1,9 +0,0 @@
-from flask_googlelogin import GoogleLogin
-from dmfg import app
-from dmfg.models import User
-
-googlelogin = GoogleLogin(app)
-
-@googlelogin.user_loader
-def load_user(userid):
-	return User.query.get_or_404(userid)
