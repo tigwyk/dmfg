a2af608ea8ce753435d6d78a06d283f0eea01443
Lee Ingram <lee@soylentcola.com>
2015-07-21 21:52:28 -0700

    Adding owned items for players (such aas ingredients, etc)

--- dmfg/models.py
+++ dmfg/models.py
@@ -108,13 +108,13 @@ class User(db.Model):
         return self.items_owned.items()
 
     def add_item(self, item_id, qty):
-        if self.items_owned.has_key(item_id)
+        if item_id in self.items_owned
             return (self.items_owned[item_id] = self.items_owned[item_id] + qty)
         else
             return (self.items_owned[item_id] = qty)
         
     def remove_item(self, item_id, qty):
-        if self.items_owned.has_key(item_id) && ((self.items_owned[item_id]-qty)>0)
+        if (item_id in self.items_owned) && ((self.items_owned[item_id]-qty)>0)
             return (self.items_owned[item_id] = self.items_owned[item_id] - qty)
         else
             return
