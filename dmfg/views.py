dc29f91296678db6920c3d468228852db808f0e8
Lee Ingram <lee@soylentcola.com>
2015-07-22 12:48:53 -0700

    Figuring out how to save a blob of data on the player db model

--- dmfg/views.py
+++ dmfg/views.py
@@ -49,7 +49,7 @@ def profile_page():
 	items_temp = current_user.get_items_owned()
 	items_table = [[]]
 	for item in items_temp.viewkeys():
-		items_table.append([Item.query.get(int(item)),qty,"javascript();"])
+		items_table.append([Item.query.get(int(item)),items_temp.get(item),"javascript();"])
 	return render_template('profile.html', items_table=items_table)
 
 @app.route('/authorize/<provider>')
