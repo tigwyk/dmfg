88329ab1e0319637c019ef5025178abc3c097478
Lee Ingram <lee@soylentcola.com>
2015-07-24 10:55:19 -0700

    Creating templates, forms, and views for user-generated trades

--- dmfg/views.py
+++ dmfg/views.py
@@ -111,7 +111,7 @@ def oauth_callback(provider):
 def flash_errors(form):
 	for field, errors in form.errors.items():
 		for error in errors:
-			flash(u"Error in the %s field - %s" % (
+			flash("Error in the %s field - %s" % (
 			        getattr(form, field).label.text,
 			        error
 			        ),"error")
