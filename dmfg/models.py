9a065b9acec538fa522ebf649d4fa8793ade8569
Lee Ingram <lee@soylentcola.com>
2015-07-24 15:30:31 -0700

    Creating tasks that process open trades

--- dmfg/models.py
+++ dmfg/models.py
@@ -29,21 +29,21 @@ class Item(db.Model):
     def human_time(self):
         return humanize.naturaltime(datetime.datetime.now()-self.created_date)
 
-#class Factory(db.Model):
+class Factory(db.Model):
     
-    #id = db.Column(db.Integer, primary_key=True)
-    #name = db.Column(db.String())
-    #capacity = db.Column(db.Integer)
-    #current_job = db.relationship("ManufactureJob", backref="factory")
-    #user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    capacity = db.Column(db.Integer)
+    current_job = db.relationship("ManufactureJob", backref="factory")
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     
-    #def __init__(self, name=None, capacity=0, owner=None):
-        #self.name = name
-        #self.capacity = capacity
-        #self.owner = owner
+    def __init__(self, name=None, capacity=0, owner=None):
+        self.name = name
+        self.capacity = capacity
+        self.owner = owner
 
-    #def __repr__(self):
-        #return '<factory id %r name=%r>' % (self.id, self.name)    
+    def __repr__(self):
+        return '<factory id %r name=%r>' % (self.id, self.name)    
 
 class Distributor(db.Model):
     
@@ -98,9 +98,9 @@ class ManufactureJob(db.Model):
     quantity = db.Column(db.Integer)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     item_id = db.Column(db.Integer, db.ForeignKey('item.id'))
-    #factory_id = db.Column(db.Integer, db.ForeignKey('factory.id'))
+    factory_id = db.Column(db.Integer, db.ForeignKey('factory.id'))
 
-    def __init__(self, name="",quantity=0,user=None,item=None):
+    def __init__(self, name="",quantity=0,user=None,item=None,factory=None):
         self.name = name
         self.quantity = quantity
         self.user = user
@@ -128,7 +128,7 @@ class User(db.Model):
     mfg_jobs = db.relationship("ManufactureJob", backref="user")
     items_owned = db.Column(JSON)
     money = db.Column(db.Float)
-    #factories = db.relationship("Factory",backref="owner")
+    factories = db.relationship("Factory",backref="owner")
     distributors = db.relationship("Distributor", backref="owner")
 
     def __init__(self, name="",email="",items_owned=dict()):
@@ -195,6 +195,6 @@ class User(db.Model):
 admin.register(User, session=db.session)
 admin.register(Item, session=db.session)
 admin.register(Trade, session=db.session)
-#admin.register(Factory, session=db.session)
+admin.register(Factory, session=db.session)
 admin.register(Distributor, session=db.session)
 admin.register(ManufactureJob, session=db.session)
