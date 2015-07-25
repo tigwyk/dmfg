606658f9014970221b897a5ac09e4272f2885ed1
Lee Ingram <lee@soylentcola.com>
2015-07-25 14:38:39 -0700

    Creating a system for auto-injecting trades until there are actual players

--- dmfg/inject_trades.py
+++ dmfg/inject_trades.py
@@ -3,7 +3,7 @@ from .database import db
 import random
 
 def create_random_trades():
-    for x in random.randrange(2,Item.query.all().count()):
+    for x in random.randrange(2,Item.query.count()):
         t = Trade(quantity=random.randrange(5,50), price=random.randrange(50,100), order_type="B", user=User.query.filter_by(name='System').first(), item=Item.query.get(x))
         db.session.add(t)
     db.session.commit()
