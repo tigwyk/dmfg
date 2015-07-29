c039e45138aa9ca4c9537c652dc11806a0101d00
Lee Ingram <lee@soylentcola.com>
2015-07-29 16:39:30 -0700

    Changing trade HTML template for funsies

--- dmfg/inject_trades.py
+++ dmfg/inject_trades.py
@@ -3,7 +3,7 @@ from .database import db
 import random
 
 def create_random_trades():
-    x = Item.query.get(random.randrange(2,Item.query.count()))
+    x = Item.query.get(random.randrange(2,Item.query.count()+1))
     t = Trade(quantity=random.randrange(5,50), price=random.randrange(50,100), order_type="B", user=User.query.filter_by(name='System').first(), item=x)
     db.session.add(t)
     db.session.commit()
