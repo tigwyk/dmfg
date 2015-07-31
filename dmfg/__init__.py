92b4abc4af496b98313ba354476f65754e4f2bc4
Lee Ingram <lee@soylentcola.com>
2015-07-30 19:33:45 -0700

    Making better trade logic and now trades above the asking price should be considered

--- dmfg/__init__.py
+++ dmfg/__init__.py
@@ -3,13 +3,12 @@ import os
 from flask import Flask
 from flask_bootstrap import Bootstrap
 import logging, sys
-from flask_superadmin import Admin
+from flask.ext.superadmin import Admin
 
 logging.basicConfig(stream=sys.stderr)
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 
 app = Flask(__name__,template_folder=tmpl_dir)
-admin = Admin(app)
 Bootstrap(app)
 
 from . import views
@@ -19,5 +18,7 @@ from . import tasks
 from . import auth
 from . import forms
 from . import inject_trades
+
+admin = Admin(app,index_view=models.MyAdminIndexView())
 
 celery = tasks.make_celery(app)
